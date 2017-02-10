3938e2428dc5f281d49c427ab0311feb7903203f
Cesar <info@ci-dd.com>
2017-02-10 12:25:21 -0500

    Cambio en user y pass para conectar a base de datos

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'antruco-admin');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'kjbdljblj890809');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
